c0335705c17816c0a711640e2773e4c1b20c37b9
Evgenii Bondarenko <gordondalos@gmail.com>
2018-09-10 12:45:57 +0600

    переключалка для правок

--- src/app/translate/xlf-transalte.module.ts
+++ src/app/translate/xlf-transalte.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 
 import { TranslateComponent } from './translate.component';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
  imports: [
    CommonModule,
+   FormsModule,
  ],
  exports: [
    TranslateComponent,
