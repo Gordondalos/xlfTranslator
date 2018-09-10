39b7ab212e457abdb8c8ce3ab466d2b0c63ed291
Evgenii Bondarenko <gordondalos@gmail.com>
2018-09-10 12:02:24 +0600

    Решен вопрос с необновляемосью данных

--- src/app/translate/translate.component.ts
+++ src/app/translate/translate.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
 import { XlfTranslatorService } from '../services/xlf-translator.service';
 import * as _ from 'lodash';
 
@@ -9,23 +9,23 @@ import * as _ from 'lodash';
   styleUrls: ['./translate.component.scss']
 })
 export class TranslateComponent implements OnInit {
+  show = false;
 
-  arrTranslates: Array<any> = [];
+  arrTranslates: any = [];
 
   constructor(
+    private ngzone: NgZone,
     private xlfTranslatorService: XlfTranslatorService,) {
   }
 
   ngOnInit() {
     this.xlfTranslatorService.sources
       .subscribe((arrTranslates) => {
-        _.each(arrTranslates, (item, index) => {
-          this.arrTranslates.push(item);
-        });
         console.log(this.arrTranslates);
-       console.log(window);
-
-
+        this.show = true;
+        this.ngzone.run(() => {
+          this.arrTranslates = arrTranslates
+        });
       })
   }
 
